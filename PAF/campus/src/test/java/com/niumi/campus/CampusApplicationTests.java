6394633f91e0c225565687339a4db1b67b50cc19
nuumi <niumimethma@outlook.com>
2026-03-04 19:29:37 +0530

    docs

---
+++ PAF/campus/src/test/java/com/niumi/campus/CampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niumi.campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
